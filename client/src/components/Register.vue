72821cab9a9a00129eb25a1df8d28a6c891b3596
Cedricium <amaya.cedric@gmail.com>
2018-01-15 01:17:41 -0800

    Setting up the Registration page + endpoint
    Also added semi-colon rule to client eslint and made necessary changes
    per linting rules.

---
+++ client/src/components/Register.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="johndoe@example.com" />
+    <br />
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="••••••••" />
+    <br />
+    <button
+      @click="register">
+      Register
+    </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService';
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      });
+      console.log(response.data);
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  margin-bottom: 10px;
+  padding: 4px 8px;
+}
+</style>
