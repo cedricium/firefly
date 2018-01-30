c80b34b4ddc185ad411d055f8fe3cce0b8e914e2
Cedricium <amaya.cedric@gmail.com>
2018-01-29 19:38:50 -0800

    Updated Header.vue to include a 'Browse' button

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="navbar is-transparent">
     <div class="navbar-brand">
-      <a class="navbar-item" href="#">
+      <router-link class="navbar-item" :to="{name: 'home'}">
         <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-      </a>
+      </router-link>
       <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
         <span></span>
         <span></span>
@@ -13,13 +13,24 @@
 
     <div id="navbarExampleTransparentExample" class="navbar-menu">
       <div class="navbar-start">
-        <router-link class="navbar-item" :to="{name: 'home'}">
-          Home
+        <router-link class="navbar-item" :to="{name: 'sites'}">
+          Browse
         </router-link>
+        <div class="navbar-item"
+          v-show="$store.state.isUserLoggedIn">
+          <div class="field is-grouped">
+            <div class="control">
+              <router-link class="button" :to="{name: ''}">
+                Add Site
+              </router-link>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="navbar-end">
-        <div class="navbar-item">
+        <div class="navbar-item"
+          v-show="!$store.state.isUserLoggedIn">
           <div class="field is-grouped">
             <p class="control">
               <router-link class="button" :to="{name: 'login'}">
@@ -43,5 +54,7 @@
 </script>
 
 <style scoped>
-
+nav.navbar {
+  box-shadow: 0 1px 1px rgba(0,0,0,.1);
+}
 </style>
