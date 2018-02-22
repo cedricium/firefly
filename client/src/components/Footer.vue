62dcb6aace74454bae6c9e247164a57ac8aec0fa
Cedricium <amaya.cedric@gmail.com>
2018-02-22 07:57:28 -0800

    Refactor code to shorten line lengths

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -15,21 +15,30 @@
         <iframe src="https://ghbtns.com/github-btn.html?user=cedricium&repo=firefly&type=star&count=true&size=large"
           frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
         <br />
-        <small>Special thanks to <span class="highlight">Cody Seibert</span> (<a href="https://www.twitter.com/codylseibert" target="_blank">@CodyLSeibert</a>) for the awesome Vue.js / Express.js tutorial.</small>
+        <small>Special thanks to <span class="highlight">Cody Seibert</span>
+          (<a href="https://www.twitter.com/codylseibert" target="_blank">@CodyLSeibert</a>)
+          for the awesome Vue.js / Express.js tutorial.
+        </small>
       </div>
       <div class="column is-4 info">
         <a href="https://bulma.io/">
           <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" height="24" width="128">
         </a>
         <br />
-        <small>Powered by <a href="https://vuejs.org" class="vue" target="_blank"><span class="green">Vue</span><span class="blue">.js</span></a></small>
+        <small>Powered by <a href="https://vuejs.org" class="vue" target="_blank">
+          <span class="green">Vue</span><span class="blue">.js</span></a>
+        </small>
         <br />
-        <small>Website content licensed <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a></small>
+        <small>Website content licensed
+          <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
+        </small>
       </div>
     </div>
     <div class="container">
       <div class="content has-text-centered">
-        <p>Made with ❤️ by <a href="http://cedricamaya.me" target="_blank">Cedric Amaya</a>. All screenshots &copy; of their respective owners.</p>
+        <p>Made with ❤️ by <a href="http://cedricamaya.me" target="_blank">Cedric Amaya</a>.
+          All screenshots &copy; of their respective owners.
+        </p>
       </div>
     </div>
   </footer>
