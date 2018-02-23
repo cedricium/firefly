e3fdc27b4fe40a4b7e8a63f4ef4fb62924f9b7e9
Cedricium <amaya.cedric@gmail.com>
2018-02-23 14:29:25 -0800

    Fix spacing in Footer with `div.container`

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,44 +1,46 @@
 <template>
   <footer class="footer">
-    <div class="columns">
-      <div class="column is-4 about">
-        <div class="logo"></div>
-        <p class="motto">Inspiring sites from around the web.</p>
-        <div class="copy">
-          <p>We aim to collect the coolest websites in order to help creatives
-            find the inspiration and motivation to make even cooler stuff.
-          </p>
+    <div class="container">
+      <div class="columns">
+        <div class="column is-4 about">
+          <div class="logo"></div>
+          <p class="motto">Inspiring sites from around the web.</p>
+          <div class="copy">
+            <p>We aim to collect the coolest websites in order to help creatives
+              find the inspiration and motivation to make even cooler stuff.
+            </p>
+          </div>
+        </div>
+        <div class="column is-4 foss">
+          <p>Firefly is <span class="highlight">open-source</span>, ⭐️ it on GitHub:</p>
+          <iframe src="https://ghbtns.com/github-btn.html?user=cedricium&repo=firefly&type=star&count=true&size=large"
+            frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+          <br />
+          <small>Special thanks to <span class="highlight">Cody Seibert</span>
+            (<a href="https://www.twitter.com/codylseibert" target="_blank">@CodyLSeibert</a>)
+            for the awesome Vue.js / Express.js tutorial.
+          </small>
+        </div>
+        <div class="column is-4 info">
+          <a href="https://bulma.io/" target="_blank">
+            <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" height="24" width="128">
+          </a>
+          <br />
+          <small>Powered by <a href="https://vuejs.org" class="vue" target="_blank">
+            <span class="green">Vue</span><span class="blue">.js</span></a>
+          </small>
+          <br />
+          <small>Website content licensed
+            <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>
+          </small>
         </div>
       </div>
-      <div class="column is-4 foss">
-        <p>Firefly is <span class="highlight">open-source</span>, ⭐️ it on GitHub:</p>
-        <iframe src="https://ghbtns.com/github-btn.html?user=cedricium&repo=firefly&type=star&count=true&size=large"
-          frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
-        <br />
-        <small>Special thanks to <span class="highlight">Cody Seibert</span>
-          (<a href="https://www.twitter.com/codylseibert" target="_blank">@CodyLSeibert</a>)
-          for the awesome Vue.js / Express.js tutorial.
-        </small>
-      </div>
-      <div class="column is-4 info">
-        <a href="https://bulma.io/" target="_blank">
-          <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" height="24" width="128">
-        </a>
-        <br />
-        <small>Powered by <a href="https://vuejs.org" class="vue" target="_blank">
-          <span class="green">Vue</span><span class="blue">.js</span></a>
-        </small>
-        <br />
-        <small>Website content licensed
-          <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>
-        </small>
-      </div>
-    </div>
-    <div class="container">
-      <div class="content has-text-centered">
-        <p>Made with ❤️ by <a href="http://cedricamaya.me" target="_blank">Cedric Amaya</a>.
-          All screenshots &copy; of their respective owners.
-        </p>
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>Made with ❤️ by <a href="http://cedricamaya.me" target="_blank">Cedric Amaya</a>.
+            All screenshots &copy; of their respective owners.
+          </p>
+        </div>
       </div>
     </div>
   </footer>
